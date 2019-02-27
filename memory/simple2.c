8ab3adbae3b6dfc8c70643d076bf656079f02a34
Linh B Ngo <linh.b.ngo@gmail.com>
2019-02-27 21:22:33 +0000

    memory

---
+++ memory/simple2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int y;
+int z = 10;
+
+int main(int argc, char *argv[]){
+  printf("location of code: %p\n", (void *) main);
+  printf("location of data: %p\n", (void *) &z);
+  printf("location of bss:  %p\n", (void *) &y);
+  printf("location of heap: %p\n", (void *) malloc(sizeof(int)));
+  printf("location of heap: %p\n", (void *) malloc(10 * sizeof(int)));
+  printf("location of heap: %p\n", (void *) malloc(sizeof(float)));
+  int x = 3;
+  printf("location of stack (x): %p\n", (void *) &x);
+  float w = 6;
+  printf("location of stack (w): %p\n", (void *) &w);
+  return 0;
+}
---
+++ memory/simple4.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int f1(void) {
+  int x = 0;
+  printf("location of f1 stack (x): %p\n", (void *) &x);
+  return 0;
+}
+
+int f2(void) {
+  int y = 0;
+  printf("location of f2 stack (y): %p\n", (void *) &y);
+  return 0;
+}
+
+int y;
+int z = 10;
+
+int main(int argc, char *argv[]){
+  printf("location of code: %p\n", (void *) main);
+  printf("location of data: %p\n", (void *) &z);
+  printf("location of bss:  %p\n", (void *) &y);
+  printf("location of heap: %p\n", (void *) malloc(sizeof(int)));
+  printf("location of heap: %p\n", (void *) malloc(10 * sizeof(int)));
+  printf("location of heap: %p\n", (void *) malloc(sizeof(float)));
+  int x = 3;
+  printf("location of stack (x): %p\n", (void *) &x);
+  float w = 6;
+  printf("location of stack (w): %p\n", (void *) &w);
+  float i = 10.0;
+  printf("location of stack (i): %p\n", (void *) &i);
+  f1();
+  f2();
+  return 0;
+}
